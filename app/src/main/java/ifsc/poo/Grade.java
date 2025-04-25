1c27746a907159d19fe5584f68c8bb5c676908f3
Igor <igor.s10@aluno.ifsc.edu.br>
2025-04-25 13:56:01 -0300

    Implementado método para imprimir os rótulos de cada quadrado do tabuleiro. Testei e funcionou (OK).

--- app/src/main/java/ifsc/poo/Grade.java
+++ app/src/main/java/ifsc/poo/Grade.java
@@ -6,6 +6,8 @@ public class Grade{
 
     public static void desenhar(Draw grade){
 
+        rotulo(grade);
+
         for(int y = 100; y < 500; y += 40) {
 
             for(int x = 100; x < 500; x += 40){
@@ -13,8 +15,19 @@ public class Grade{
                 grade.square((double) x, (double) y, 20.0f);
 
             }
-
         }
+    }
+
+    private static void rotulo(Draw grade){
+
+        char letra = 'A';
+        char numero = '0';
+
+        for(int i = 100; i < 500; i += 40)
+            grade.text(i, 50, (numero++) + "", 20);
+
+        for(int i = 100; i < 500; i += 40)
+            grade.text(50, i, (letra++) + "", 20);
 
     }
 
